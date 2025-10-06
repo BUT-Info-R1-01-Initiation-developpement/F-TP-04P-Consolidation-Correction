2b875be27b6a477eceb187a8b0bc914904721756
Franck Silvestre <franck.silvestre@gmail.com>
2025-10-06 16:47:01 +0200

    fix #5 Des magiques commentés

---
+++ src/main/kotlin/tp04/activite/d/DesMagiques.kt
@@ -0,0 +1,13 @@
+package tp04.activite.d
+
+fun main() {
+    // Faire
+    //   - recuperer la valeur des dés du joueur
+    //   - Afficher les valeurs des dés du joueur
+    //   - recuperer la valeur des dés de l'ordinateur
+    //   - Afficher les valeurs des dés de l'ordinateur
+    //   - déterminer le vainqueur
+    //   - afficher les informations sur le gagnant
+    //   - demander au joueur s'il veut jouer encore
+    // Tant que le joueur veut jouer
+}
